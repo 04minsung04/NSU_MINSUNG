d57180056644e2d3058afdd003e5d3fda6339f7e
04minsung04 <162725136+04minsung04@users.noreply.github.com>
2024-03-20 11:31:57 +0900

    Create p.99 BreakDemo.java

---
+++ 3월 20일/p.99 BreakDemo.java
@@ -0,0 +1,17 @@
+package chap01;
+
+public class chap06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1, j = 5;
+		
+		while(true)
+		{
+			System.out.print(i++);
+			if(i >= j)
+				break;
+		}
+	}
+
+}
